7c3b42a6d2db78d6b38f33f6fba6467a1a0227fe
MrCrankHank <mail@alexander-hank.de>
2015-11-27 21:37:40 +0100

    Add example

--- shake.js
+++ shake.js
@@ -1,29 +1,31 @@
-$.fn.vibrate = function(conf) {
-    var config = $.extend({
-        speed: 30,
-        duration: 2000,
-        spread: 3
-    }, conf);
+(function($){
+    $.fn.shake = function(conf) {
+        var config = $.extend({
+            speed: 30,
+            duration: 2000,
+            spread: 3
+        }, conf);
 
-    return this.each(function() {
-        var t = $(this);
+        return this.each(function() {
+            var t = $(this);
 
-        var vibrate = function() {
-            var topPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
-            var leftPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
-            var rotate = Math.floor(Math.random() * config.spread - (config.spread - 1) / 2); // cheers to erik@birdy.nu for the rotation-idea
-            t.css({position: 'relative', left: leftPos +'px', top: topPos +'px', WebkitTransform: 'rotate(' +rotate +'deg)'});
-        };
-
-        var doVibration = function () {
-            var vibrationInterval = setInterval(vibrate, config.speed);
-            var stopVibration = function() {
-                clearInterval(vibrationInterval);
-                t.css({position: 'static', left: 'auto', top: 'auto', WebkitTransform: 'rotate(0deg)'});
+            var shake = function() {
+                var topPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+                var leftPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+                var rotate = Math.floor(Math.random() * config.spread - (config.spread - 1) / 2); // cheers to erik@birdy.nu for the rotation-idea
+                t.css({position: 'relative', left: leftPos +'px', top: topPos +'px', WebkitTransform: 'rotate(' +rotate +'deg)'});
             };
-            setTimeout(stopVibration, config.duration);
-        };
 
-        doVibration();
-    });
-};
+            var doShake = function () {
+                var vibrationInterval = setInterval(shake, config.speed);
+                var stopShake = function() {
+                    clearInterval(vibrationInterval);
+                    t.css({position: 'static', left: 'auto', top: 'auto', WebkitTransform: 'rotate(0deg)'});
+                };
+                setTimeout(stopShake, config.duration);
+            };
+
+            doShake();
+        });
+    };
+}( jQuery ));
